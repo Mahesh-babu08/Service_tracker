103e155ca2d0a92da126fdaa1460eb645a7863fe
Mahesh Babu <196810300+Mahesh-babu08@users.noreply.github.com>
2026-04-06 21:25:36 +0530

    Docker written

--- backend/src/main/java/com/enterprise/service_tracker/TicketAnalyzerApplication.java
+++ backend/src/main/java/com/enterprise/service_tracker/TicketAnalyzerApplication.java
@@ -3,10 +3,15 @@ package com.enterprise.service_tracker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class TicketAnalyzerApplication {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
 		SpringApplication.run(TicketAnalyzerApplication.class, args);
 	}
 
